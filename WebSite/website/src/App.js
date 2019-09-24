1022b73f20f335d9c89215547f0c8e9298d3412d
Gogul <gogulkannan@sg.kapps.com>
2019-09-24 21:46:46 +0800

    react base'

---
+++ WebSite/website/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="pimg1">
+        <div className="ptext">
+          <span className="border">
+            Parallax Website
+          </span>
+        </div>
+      </div>
+      <section className="section section-light">
+        <h2>Section One</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt, laudantium, quibusdam? Nobis, delectus, commodi, fugit amet tempora facere dolores nisi facilis consequatur, odio hic minima nostrum. Perferendis eos earum praesentium, blanditiis sapiente labore aliquam ipsa architecto vitae. Minima soluta temporibus voluptates inventore commodi cumque esse suscipit optio aliquam et, dolorem a cupiditate nihil fuga laboriosam fugiat placeat dignissimos! Unde eveniet placeat quisquam blanditiis voluptatem doloremque fugiat dolor repellendus ratione in. Distinctio provident dolorem modi cumque illo enim quidem tempora deserunt nostrum voluptate labore repellat quisquam quasi cum suscipit dolore ab consequuntur, ad porro earum temporibus. Laborum ad temporibus ex, omnis!
+    </p>
+      </section>
+      <div className="pimg2">
+        <div className="ptext">
+          <span className="border trans">
+            Image Two Text
+      </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
